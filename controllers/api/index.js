f4a1ff68a66c52359146995673ce055e5c3657e6
WMABABB <wm.andrew.babb@gmail.com>
2018-09-12 17:04:45 -0400

    updating api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.status(200).send('<a href=\'/api/articles/\'>articles</a><br><a href=\'/api/notes/\'>notes</a>');
+    res.status(200).send('<a href=\'/api/articles/\'>View Articles</a><br><a href=\'/api/notes/\'>View Notes</a>');
 });
 
 router.use('/articles', require('./articles'));
